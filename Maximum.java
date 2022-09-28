53d9cba5d70146e52157db90318419d602f1e0a9
Ashutosh <ashu.cse.sln@gmail.com>
2022-09-28 18:36:21 +0530

    TC3  for UC2

--- Maximum.java
+++ Maximum.java
@@ -3,6 +3,6 @@ package org.example;
 public class Maximum {
     public static void main(String[] args) {
         Operation <Float> in =  new Operation<>();
-        in.Maximum(22.1f,29.3f,11.3f);
+        in.Maximum(22.1f,29.3f,31.3f);
     }
 }
